08795bc29c5d307dd7399f780d5d0cd2f43c9704
Edvin Honda <edvin_honda@hotmail.com>
2025-09-27 09:42:09 -0400

    fix bug i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import i18n from './plugins/i18n'
-import { createApp } from 'vue'
 
 const app = createApp(App)
 app.use(i18n)
