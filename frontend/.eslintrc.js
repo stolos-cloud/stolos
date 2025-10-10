d563f783a14421bc48ceef3a3f9c0824432eb4c2
SonOfLope <jonathanlopez@hotmail.ca>
2025-10-09 21:09:59 -0400

    js lint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,17 +1,13 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-  ],
-  rules: {
-    'vue/no-multiple-template-root': 'off',
-    'vue/require-name': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/valid-v-slot': 'off'
-  }
-}
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+    rules: {
+        'vue/no-multiple-template-root': 'off',
+        'vue/require-name': 'off',
+        'vue/multi-word-component-names': 'off',
+        'vue/valid-v-slot': 'off',
+    },
+};
