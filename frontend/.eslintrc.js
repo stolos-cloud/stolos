7272a8b5a02dcbd2539d0a6b46e999fa0a744394
SonOfLope <jonathanlopez@hotmail.ca>
2025-10-09 20:15:16 -0400

    for peace of mind

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'vue/no-multiple-template-root': 'off',
