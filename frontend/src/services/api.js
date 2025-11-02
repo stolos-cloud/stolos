dc9a2f0b42b4ac9fb323362d07e54e44cfdd54df
Michael Sakellaropoulos <mjsakellaropoulos@gmail.com>
2025-11-02 13:39:13 -0500

    Add api version const

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -34,6 +34,7 @@ api.interceptors.response.use(
     }
 );
 
-api.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
+export const API_BASE_VERSION = "/api/v1"
+api.defaults.baseURL = ( import.meta.env.VITE_API_BASE_URL + API_BASE_VERSION )
 
 export default api;
