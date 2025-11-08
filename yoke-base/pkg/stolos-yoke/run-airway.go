6e2d42e1838b32c0698fcd1d8a21e5ce508b16dc
Simon-Boyer <si.boyer@hotmail.ca>
2025-11-08 15:32:42 -0500

    yoke base

---
+++ yoke-base/pkg/stolos-yoke/run-airway.go
@@ -0,0 +1,25 @@
+package stolos_yoke
+
+import (
+	"flag"
+	"os"
+)
+
+func Run[flightResource any](inputs AirwayInputs, renderTemplateFunc func() ([]byte, error)) {
+
+	flightUrl := flag.String("flight-url", "", "flight url")
+	flag.Parse()
+
+	if *flightUrl == "" {
+		panic("flight url is required")
+	}
+
+	airway, err := BuildAirwayFor[flightResource](inputs, *flightUrl)
+
+	if err != nil {
+		panic(err)
+	}
+	if _, err := os.Stdout.Write(airway); err != nil {
+		panic(err)
+	}
+}
